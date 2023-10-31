a758b8c5c70be53fa7a74e428e5de84544f55a8e
tonhat211 <21130463@st.hcmuaf.edu.vn>
2023-10-31 18:57:41 +0700

    lab 4

---
+++ lab4/src/lab4/AStarSearchAlgo.java
@@ -0,0 +1,105 @@
+package lab4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class AStarSearchAlgo implements IInformedSearchAlgo {
+
+	@Override
+	public Node execute(Node root, String goal) {
+		// TODO Auto-generated method stub
+//		PriorityQueue<Node> froniter = new PriorityQueue<>(new NodeComparator());
+		PriorityQueue<Node> froniter = new PriorityQueue<>(new NodeComparatorByHn());
+		List<Node> explored = new ArrayList<Node>();
+		froniter.add(root);
+		while(!froniter.isEmpty()) {
+			Node checking = froniter.poll();
+			explored.add(checking);
+			if(checking.getLabel().equals(goal))
+				return checking;
+			else {
+				List<Edge> edges = checking.getChildren();
+//				List<Node> subs = checking.getChildrenNodes();
+				for(Edge e: edges) {
+//					froniter.add(n);
+				
+					Node end = e.getEnd();
+					double h = checking.getH();
+
+
+					if(!froniter.contains(end) && !explored.contains(end)) {
+						froniter.add(end);
+						end.setParent(checking);
+						end.setG(checking.getG() + e.getWeight());
+
+						
+					}
+					else if (froniter.contains(end) && end.getG() > (checking.getG() + e.getWeight())) {
+						end.setG(checking.getG() + e.getWeight());
+						end.setParent(checking);
+
+//						end.setPathCost(newPathCost);
+
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Node execute(Node root, String start, String goal) {
+		// TODO Auto-generated method stub
+		boolean started=false;
+		PriorityQueue<Node> frontier = new PriorityQueue<>(new NodeComparatorByHn());
+		frontier.add(root);
+		List<Node> explored = new ArrayList<Node>();
+		while(!frontier.isEmpty()) {
+			Node checking = frontier.poll();
+			explored.add(checking);
+			
+			if(checking.getLabel().equals(start)) {
+				started=true;
+				frontier.clear();
+				explored.clear();
+				checking.setParent(null);
+				
+			}
+			
+			if(checking.getLabel().equals(goal) && started==true) {
+
+				return checking;
+			}
+			
+			List<Edge> edges = checking.getChildren();
+//			List<Node> subs = checking.getChildrenNodes();
+			for(Edge e: edges) {
+//				froniter.add(n);
+			
+				Node end = e.getEnd();
+				double h = checking.getH();
+
+
+				if(!frontier.contains(end) && !explored.contains(end)) {
+					frontier.add(end);
+					end.setParent(checking);
+					end.setG(checking.getG() + e.getWeight());
+
+					
+				} else if (frontier.contains(end) && end.getG() > (checking.getG() + e.getWeight())) {
+					end.setG(checking.getG() + e.getWeight());
+					end.setParent(checking);
+
+
+				}
+			}
+				
+			
+			
+		}
+		return null;
+	}
+
+}
